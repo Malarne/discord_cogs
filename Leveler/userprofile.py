6b8b77ac64f84c203e62f26188a90174081cb9e1
Malarne <34847642+Malarne@users.noreply.github.com>
2019-02-28 23:54:07 +0100

    Update userprofile.py

--- Leveler/userprofile.py
+++ Leveler/userprofile.py
@@ -89,7 +89,7 @@ class UserProfile:
     async def _check_role_member(self, member):
         roles = await self.data.guild(member.guild).roles()
         lvl = await self.data.member(member).level()
-        level_role = if lvl > 0 roles[(lvl%10)-1] else 0
+        level_role = roles[(lvl%10)-1] if lvl > 0 else 0
         try:
             if level_role in [x.id for x in member.roles]:
                 return True
