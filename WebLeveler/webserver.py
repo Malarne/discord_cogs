b3895c67106a4673c2cc11655835235a85e89d28
orels1 <orel-s1@yandex.ru>
2019-03-13 02:09:54 +0300

    reworked ui and backend a bit

--- WebLeveler/webserver.py
+++ WebLeveler/webserver.py
@@ -1,11 +1,12 @@
 from aiohttp import web
+import json
 import asyncio
 import os
 from shutil import copyfile
 import discord
 
-class WebServer:
 
+class WebServer:
     def __init__(self, bot):
         self.app = web.Application()
         self.bot = bot
@@ -20,7 +21,7 @@ class WebServer:
     def __unload(self):
         self.bot.loop.create_task(self.runner.cleanup())
 
-    def get_body(self, path, reset : bool = False):
+    def get_body(self, path, reset: bool = False):
         if self.body is None or reset == True:
             folder = path[0]
             basic = os.path.join(folder, "index.html")
@@ -33,36 +34,47 @@ class WebServer:
     async def make_webserver(self, path):
         cog = self.bot.get_cog("Leveler")
         stats = cog.profiles
+
         async def page(request):
+            body = self.get_body(path, True)
+            self.body = body
+            return web.Response(text=body, content_type="text/html")
+
+        # expects a { results: [ <server1>, <server2> ] } response
+        # server object shape example - in frontend code
+        async def list(requst):
             data = {}
-            res = "<p id='datadiv'>"
+            response = []
             for i in self.bot.guilds:
                 data[i.id] = {}
                 data[i.id] = await stats._get_leaderboard(i)
             for i in data:
-                res += "<h3>Serveur: " + self.bot.get_guild(i).name + "</h3><br />"
+                serverUsers = []
                 for j in data[i]:
-                    user = self.bot.get_user(j["id"])
-                    userxp = j["xp"]
-                    userlevel = j["lvl"]
-                    res += f"{user.display_name}: Niveau {userlevel} avec {userxp} XP !<br />"
-                res += "</p><br /><p id='datadiv'>"
-            res += "</p>"
-            body = self.get_body(path, True)
-            body = body.replace("[[]]", res)
-            body = body.replace("<div id='DataContainer' style='display: none'>", "<div id='DataContainer' style='display: inline-block'>")
-            self.body = body
-            return web.Response(text=body, content_type='text/html')
+                    serverUsers.append(
+                        {
+                            "name": self.bot.get_user(j["id"]).display_name,
+                            "xp": j["xp"],
+                            "lvl": j["lvl"],
+                        }
+                    )
 
+                response.append(
+                    {"name": self.bot.get_guild(i).name, "users": serverUsers}
+                )
+            body = json.dumps({"results": response})
+            return web.Response(text=body, content_type="application/json")
+
+        # expects a { result: <userData> } or { error: <errorString> }
+        # example object in the frontend code
         async def userinfo(request):
-            data = await request.post()
-            body = self.get_body(path)
-            usr = data["user"]
+            usr = request.rel_url.query["name"]
+            resp = {}
             srch = discord.utils.get(self.bot.get_all_members(), name=usr)
             if srch is None:
                 srch = discord.utils.get(self.bot.get_all_members(), id=int(usr))
             if srch is None:
-                res = "Utilisateur inconnu"
+                resp["error"] = "Utilisateur inconnu"
             else:
                 info = await cog.profile_data(srch)
                 xp = info["xp"]
@@ -76,10 +88,12 @@ class WebServer:
                     elo = default if default else cog.defaultrole
                 else:
                     if str(lvl) in roles.keys():
-                        elo = discord.utils.get(srch.guild.roles, id=roles[str(lvl)]).name
+                        elo = discord.utils.get(
+                            srch.guild.roles, id=roles[str(lvl)]
+                        ).name
                     else:
                         tmp = 0
-                        for k,v in roles.items():
+                        for k, v in roles.items():
                             if int(k) < lvl:
                                 tmp = int(v)
                                 pass
@@ -88,20 +102,27 @@ class WebServer:
                         else:
                             rl = discord.utils.get(srch.guild.roles, id=tmp)
                             elo = rl.name
-                res = f"<p><br />Niveau: {lvl}<br />Exp: {xp} / {nxp}<br />Elo: {elo}<br />Classement: {ldb}<br /><br />{desc}</p>"
-                body = body.replace("<div id='dialog' class='nes-container' style='display: none'>", f"<div id='dialog' class='nes-container' title='{srch.display_name}' style='display: inline-block'>")
-                body = body.replace("(())", res)
-                body = body.replace("$(function() {$( '#dialog' ).hide();});", "<!--We no more hide our div ! --!>")
-                return web.Response(text=body, content_type='text/html')
-            body = body.replace("(())", res)
-            return web.Response(text=body, content_type='text/html')
+                resp = {
+                    "results": {
+                        "lvl": lvl,
+                        "xp": xp,
+                        "nxp": nxp,
+                        "elo": elo,
+                        "ldb": ldb,
+                        "desc": desc,
+                    }
+                }
+            body = json.dumps(resp)
+            return web.Response(text=body, content_type="application/json")
 
         await asyncio.sleep(3)
-        self.app.router.add_get('/', page)
-        self.app.router.add_post('/', userinfo)
+        self.app.router.add_get("/", page)
+        self.app.router.add_get("/list", list)
+        self.app.router.add_get("/info", userinfo)
         self.runner = web.AppRunner(self.app)
         await self.runner.setup()
         self.handler = self.app.make_handler(debug=True)
-        self.site = web.TCPSite(self.runner, '0.0.0.0', self.port)
+        self.site = web.TCPSite(self.runner, "0.0.0.0", self.port)
         await self.site.start()
-        print('WebTest started ...')
+        print("WebTest started ...")
+
