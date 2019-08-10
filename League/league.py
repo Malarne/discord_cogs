aba682bfd59e29fe93fe3714d4a6b12ea8dd62de
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-10 22:52:57 +0100

    Update league.py

--- League/league.py
+++ League/league.py
@@ -52,7 +52,7 @@ class League(commands.Cog):
             embed = discord.Embed(title="League elo", color=7802332)
             embed.add_field(name="Summoner", value=summoner, inline=True)
             embed.add_field(name="Stats", value="\n".join(res), inline=False)
-            embed.set_thumbnail(url="https://banner2.kisspng.com/20180926/igw/kisspng-league-of-legends-esports-logo-font-game-v-v-pls173-5bab06e810b838.0162687215379350800685.jpg")
+            embed.set_thumbnail(url="http://www.pngmart.com/files/3/League-of-Legends-Logo-Transparent-Background.png")
             await ctx.send(embed=embed)
         else:
             embed = discord.Embed(title="League elo", color=7802332)
